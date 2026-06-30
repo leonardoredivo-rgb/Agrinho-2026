39bbec8b734c80e2215d671259b4e220bc6dc456
leonardoredivo-rgb <leonardo.redivo@escola.pr.gov.br>
2026-06-29 21:53:29 -0300

    fdd

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-
-const confirmar = document.getElementById('botao-enviar');
-const botao = document.getElementById('botao-comecar');
-botao.forEach(button =>{
-   button.addEventListener('click', function(){
-    const atual = document.querySelector('.ativo');
-   })
-})
+const botaoEnviar = document.getElementById('botao-enviar');
 const campoNome = document.getElementById('nome');
 const textoMensagem = document.getElementById('mensagem');
 
+const telaHome = document.getElementById('tela-home');
+const telaPergunta = document.getElementById('tela-pergunta');
+const nomeJogador = document.getElementById('nome-jogador');
 
-botao.addEventListener('click', function(evento) {
-    evento.preventDefault();
-    
+// Evento de clique para mudar de tela
+botaoEnviar.addEventListener('click', function() {
     const nome = campoNome.value.trim();
     
     if (nome !== "") {
-        // Atualiza o texto da tela com o nome digitado!
-        textoMensagem.innerHTML = `Olá, <strong>${nome}</strong>!<br>Clique abaixo para começar o quiz.`;
+        // 1. Passa o nome digitado para a tela da pergunta
+        nomeJogador.innerText = nome;
+        
+        // 2. Esconde a tela inicial e mostra a tela da pergunta
+        telaHome.classList.add('escondido');
+        telaPergunta.classList.remove('escondido');
     } else {
         alert("Por favor, digite seu nome primeiro!");
     }
